ff536eaae7bc286a30cbcd6f0363e0a83d85d6c9
RCDalton <99426528+RCDalton@users.noreply.github.com>
2026-08-07 22:17:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE='tmi-v1';
-const FILES=['./','./index.html','./data.js','./manifest.webmanifest'];
+const CACHE='tmi-v2';
+const FILES=[
+  './',
+  './index.html',
+  './data.js',
+  './manifest.webmanifest',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png'
+];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
